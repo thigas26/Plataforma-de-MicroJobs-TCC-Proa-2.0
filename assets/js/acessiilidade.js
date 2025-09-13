aabb4f8c2dae1b91c64044567bf9580316faa305
Amanda Carvalho <amanda.s.carvalho15@gmail.com>
2025-09-13 19:51:43 -0300

    organização das pastas e add acessibilidade

--- assets/js/acessiilidade.js
+++
@@ -1,4 +0,0 @@
-// acessibilidade.js
-// Autores: Leandro Oliveira, Thiago Rocha, Pedro Henrique, Amanda Santos, Paulo Henrique, Willian Carvalho e João Alberto
-// Funções: contraste, ajuste de fonte, leitor de voz, persistência no localStorage
-//Uso: adicionsar <script
---
+++ js/acessibilidade.js
@@ -0,0 +1,32 @@
+// acessibilidade.js
+// Autores: Leandro Oliveira, Thiago Rocha, Pedro Henrique, Amanda Santos, Paulo Henrique, Willian Carvalho e João Alberto
+// Funções: contraste, ajuste de fonte, leitor de voz, persistência no localStorage
+//Uso: adicionsar <script
+// acessibilidade.js
+// Autor: Grupo 5
+// Alternar alto contraste
+function toggleContraste(){
+    document.body.classList.toggle ("alto-contraste");
+
+}
+//Aumentar ou diminuir fonte
+function alterarFonte(acao) {
+    const body = document.body;
+    let tamanhoAtual = parseFloat(window.getComputedStyle(body).fontSize);
+
+    if (acao === "aumentar" && tamanhoAtual < 30) {
+        body.style.fontSize = (tamanhoAtual + 2) + "px";
+    } else if (acao === "diminuir" && tamanhoAtual > 12) {
+        body.style.fontSize = (tamanhoAtual - 2) + "px";
+    }
+}
+
+//Ler texto com voz
+function lerTexto(seletor){ const elemento = document.querySelector(seletor);
+    if(elemento) {
+      const mensagem = new SpeechSynthesisUtterance(elemento.innerText);
+      mensagem.lang = "pt-BR";
+      speechSynthesis.speak(mensagem);  
+     }
+
+}
